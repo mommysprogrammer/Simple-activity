7ec734ddea6726fac8e24284e242759628f08bed
Дмитрий Вьюник <vyunik-ds@sber-zvuk.com>
2022-10-17 15:05:27 +0300

    first commit

---
+++ app/src/main/java/com/example/simpleactivity/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.simpleactivity
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
